37cd9b77a91016910bbeb36b4fb29b8d971cbd3c
baerma <max.baer@swisstph.ch>
2025-02-10 14:14:26 +0100

    Commit after review

---
+++ Phylogenetic Analysis/Amino Acid Tree/BEAST.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --job-name=TrichurisBeastMax                  #This is the name of your job
+#SBATCH --cpus-per-task=32                  #This is the number of cores reserved
+#SBATCH --mem-per-cpu=8G              #This is the memory reserved per core.
+#Total memory reserved: 156GB
+
+#SBATCH --time=1-00:00:00        #This is the time that your task will run
+#SBATCH --qos=1day           #You will run in this queue
+
+# Paths to STDOUT or STDERR files should be absolute or relative to current working directory
+#SBATCH --output=/scicore/home/schpie00/baer0006/Trichuris_Trichiura_CI/playground/protein_phylo/stdout_3    #These are the STDOUT and STDERR files
+#SBATCH --error=/scicore/home/schpie00/baer0006/Trichuris_Trichiura_CI/playground/protein_phylo/stderr_3
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=max.baer@swisstph.ch        #You will be notified via email when your task ends or fails
+
+#This job runs from the current working directory
+
+
+#Remember:
+#The variable $TMPDIR points to the local hard disks in the computing nodes.
+#The variable $HOME points to your home directory.
+#The variable $SLURM_JOBID stores the ID number of your job.
+
+
+#load your required modules below
+ml load beagle-lib/3.1.2-GCCcore-10.3.0  
+#export your required environment variables below
+#################################################
+
+
+#add your command lines below
+/scicore/home/schpie00/baer0006/Trichuris_Trichiura_CI/Nextflow/beast/beast/bin/beast -overwrite -threads 32 Beauti_file_protein_tree.xml
---
+++ Phylogenetic Analysis/Nucleic Acid Tree/Treeannotator.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name=TrichurisBeastMax                  #This is the name of your job
+#SBATCH --cpus-per-task=10                  #This is the number of cores reserved
+#SBATCH --mem-per-cpu=8G              #This is the memory reserved per core.
+#Total memory reserved: 80GB
+
+#SBATCH --time=00:30:00        #This is the time that your task will run
+#SBATCH --qos=30min           #You will run in this queue
+
+# Paths to STDOUT or STDERR files should be absolute or relative to current working directory
+#SBATCH --output=/scicore/home/schpie00/baer0006/Trichuris_Trichiura_CI/playground/nucleic_acid_phylo/stdout_Mytree2    #These are the STDOUT and STDERR files
+#SBATCH --error=/scicore/home/schpie00/baer0006/Trichuris_Trichiura_CI/playground/nucleic_acid_phylo/stderr_Mytree2
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=max.baer@swisstph.ch        #You will be notified via email when your task ends or fails
+
+#This job runs from the current working directory
+
+
+#Remember:
+#The variable $TMPDIR points to the local hard disks in the computing nodes.
+#The variable $HOME points to your home directory.
+#The variable $SLURM_JOBID stores the ID number of your job.
+
+
+#load your required modules below
+
+
+
+#export your required environment variables below
+#################################################
+
+
+#add your command lines below
+/scicore/home/schpie00/baer0006/Trichuris_Trichiura_CI/Nextflow/beast/beast/bin/treeannotator -lowMem Nucleic_acid_mito_tree-Mitochondrial_nucleic_acid_tree.trees Mito_nucl_all.tree 
